12ff785e9e3522e162b18310c89365ea38f3a298
Martin Amm <ma@adenin.com>
2020-07-27 13:04:44 -0400

    handle duplicate keys

--- src/Common/Code/Hash.cs
+++ src/Common/Code/Hash.cs
@@ -43,7 +43,7 @@ namespace DotLiquid
         private static void FromAnonymousObject(object anonymousObject, Hash hash, bool includeBaseClassProperties)
         {
             Action<object, Hash> mapper = GetObjToDictionaryMapper(anonymousObject.GetType(), includeBaseClassProperties);
-            mapper.Invoke(anonymousObject, hash);                
+            mapper.Invoke(anonymousObject, hash);
         }
 
         private static Action<object, Hash> GetObjToDictionaryMapper(Type type, bool includeBaseClassProperties)
@@ -75,7 +75,8 @@ namespace DotLiquid
             return mapper;
         }
 
-        private static void AddBaseClassProperties(Type type, List<PropertyInfo> propertyList) {
+        private static void AddBaseClassProperties(Type type, List<PropertyInfo> propertyList)
+        {
             propertyList.AddRange(type.GetTypeInfo().BaseType.GetTypeInfo().DeclaredProperties
                 .Where(p => p.CanRead && p.GetMethod.IsPublic && !p.GetMethod.IsStatic).ToList());
         }
@@ -86,10 +87,10 @@ namespace DotLiquid
             ParameterExpression hashParam = Expression.Parameter(typeof(Hash), "hashParam");
             List<Expression> bodyInstructions = new List<Expression>();
 
-            var castedObj = Expression.Variable(type,"castedObj");
-            
+            var castedObj = Expression.Variable(type, "castedObj");
+
             bodyInstructions.Add(
-                Expression.Assign(castedObj,Expression.Convert(objParam,type))
+                Expression.Assign(castedObj, Expression.Convert(objParam, type))
             );
 
             //Add properties
@@ -106,19 +107,19 @@ namespace DotLiquid
                         Expression.MakeIndex(
                             hashParam,
                             typeof(Hash).GetTypeInfo().GetDeclaredProperty("Item"),
-                            new []{Expression.Constant(property.Name, typeof(string))}
+                            new[] { Expression.Constant(property.Name, typeof(string)) }
                         ),
                         Expression.Convert(
-                            Expression.Property(castedObj,property),
+                            Expression.Property(castedObj, property),
                             typeof(object)
                         )
                     )
                 );
             }
 
-            var body = Expression.Block(typeof(void),new []{castedObj},bodyInstructions);
+            var body = Expression.Block(typeof(void), new[] { castedObj }, bodyInstructions);
 
-            var expr = Expression.Lambda < Action<object, Hash>>(body, objParam, hashParam);
+            var expr = Expression.Lambda<Action<object, Hash>>(body, objParam, hashParam);
 
             return expr.Compile();
         }
@@ -126,19 +127,19 @@ namespace DotLiquid
         public static Hash FromDictionary(IDictionary<string, object> dictionary)
         {
             Hash result = new Hash();
-
+            //System.Diagnostics.Debugger.Break();
             foreach (var keyValue in dictionary)
             {
-                    if (keyValue.Value is IDictionary<string, object>)
-                    {
-                        result.Add(keyValue.Key, FromDictionary((IDictionary<string, object>) keyValue.Value));
-                    }
-                    else
-                    {
-                        result.Add(keyValue);
-                    }
+                if (keyValue.Value is IDictionary<string, object>)
+                {
+                    result[keyValue.Key] = FromDictionary((IDictionary<string, object>)keyValue.Value);
+                }
+                else
+                {
+                    result[keyValue.Key] = keyValue.Value;
+                }
             }
-                
+
             return result;
         }
 
@@ -187,7 +188,7 @@ namespace DotLiquid
 
         public T Get<T>(string key)
         {
-            return (T) this[key];
+            return (T)this[key];
         }
 
         #region IDictionary<string, object>
@@ -199,7 +200,7 @@ namespace DotLiquid
 
         public void Remove(object key)
         {
-            ((IDictionary) _nestedDictionary).Remove(key);
+            ((IDictionary)_nestedDictionary).Remove(key);
         }
 
         object IDictionary.this[object key]
@@ -208,9 +209,9 @@ namespace DotLiquid
             {
                 if (!(key is string))
                     throw new NotSupportedException();
-                return GetValue((string) key);
+                return GetValue((string)key);
             }
-            set { ((IDictionary) _nestedDictionary)[key] = value; }
+            set { ((IDictionary)_nestedDictionary)[key] = value; }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -220,17 +221,17 @@ namespace DotLiquid
 
         public void Add(KeyValuePair<string, object> item)
         {
-            ((IDictionary<string, object>) _nestedDictionary).Add(item);
+            ((IDictionary<string, object>)_nestedDictionary).Add(item);
         }
 
         public virtual bool Contains(object key)
         {
-            return ((IDictionary) _nestedDictionary).Contains(key);
+            return ((IDictionary)_nestedDictionary).Contains(key);
         }
 
         public void Add(object key, object value)
         {
-            ((IDictionary) _nestedDictionary).Add(key, value);
+            ((IDictionary)_nestedDictionary).Add(key, value);
         }
 
         public void Clear()
@@ -240,22 +241,22 @@ namespace DotLiquid
 
         IDictionaryEnumerator IDictionary.GetEnumerator()
         {
-            return ((IDictionary) _nestedDictionary).GetEnumerator();
+            return ((IDictionary)_nestedDictionary).GetEnumerator();
         }
 
         public bool Contains(KeyValuePair<string, object> item)
         {
-            return ((IDictionary<string, object>) _nestedDictionary).Contains(item);
+            return ((IDictionary<string, object>)_nestedDictionary).Contains(item);
         }
 
         public void CopyTo(KeyValuePair<string, object>[] array, int arrayIndex)
         {
-            ((IDictionary<string, object>) _nestedDictionary).CopyTo(array, arrayIndex);
+            ((IDictionary<string, object>)_nestedDictionary).CopyTo(array, arrayIndex);
         }
 
         public bool Remove(KeyValuePair<string, object> item)
         {
-            return ((IDictionary<string, object>) _nestedDictionary).Remove(item);
+            return ((IDictionary<string, object>)_nestedDictionary).Remove(item);
         }
 
         #endregion
@@ -264,7 +265,7 @@ namespace DotLiquid
 
         public void CopyTo(Array array, int index)
         {
-            ((IDictionary) _nestedDictionary).CopyTo(array, index);
+            ((IDictionary)_nestedDictionary).CopyTo(array, index);
         }
 
         public int Count
@@ -274,27 +275,27 @@ namespace DotLiquid
 
         public object SyncRoot
         {
-            get { return ((IDictionary) _nestedDictionary).SyncRoot; }
+            get { return ((IDictionary)_nestedDictionary).SyncRoot; }
         }
 
         public bool IsSynchronized
         {
-            get { return ((IDictionary) _nestedDictionary).IsSynchronized; }
+            get { return ((IDictionary)_nestedDictionary).IsSynchronized; }
         }
 
         ICollection IDictionary.Values
         {
-            get { return ((IDictionary) _nestedDictionary).Values; }
+            get { return ((IDictionary)_nestedDictionary).Values; }
         }
 
         public bool IsReadOnly
         {
-            get { return ((IDictionary<string, object>) _nestedDictionary).IsReadOnly; }
+            get { return ((IDictionary<string, object>)_nestedDictionary).IsReadOnly; }
         }
 
         public bool IsFixedSize
         {
-            get { return ((IDictionary) _nestedDictionary).IsFixedSize; }
+            get { return ((IDictionary)_nestedDictionary).IsFixedSize; }
         }
 
         public bool ContainsKey(string key)
@@ -330,7 +331,7 @@ namespace DotLiquid
 
         ICollection IDictionary.Keys
         {
-            get { return ((IDictionary) _nestedDictionary).Keys; }
+            get { return ((IDictionary)_nestedDictionary).Keys; }
         }
 
         public ICollection<object> Values
